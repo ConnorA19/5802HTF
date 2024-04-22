c1a6d12903cb287e3bb12dc7e25dc45d043bacc6
Connor Antony <connor.antony19@gmail.com>
2024-04-22 01:10:36 -0500

    Some more HUnit

--- Tests/HUnit.hs
+++ Tests/HUnit.hs
@@ -9,9 +9,21 @@ vectorVal = vectorMaker 100
 
 test_Asserts :: Assertion
 test_Asserts = do
+    --Should Pass
     assertEqual (head vectorVal) 1
     assertBool ((head vectorVal) == 1)
+    assertNotEqual(head vectorVal) 2
+    assertListsEqualAsSets vectorVal [div (x * (x+1)) 2 | x <- [1..100]] 
+    assertNotEmpty vectorVal
+    assertEmpty []
+    assertElem 10 vectorVal
+    -- assertThrows (div 1 0) (Exception "divide by zero")
 
     --Should Fail
-    assertEqual (head vectorVal) 1
-
+    assertEqual (head vectorVal) 2
+    assertBool ((head vectorVal) == 2)
+    assertNotEqual(head vectorVal) 1
+    assertListsEqualAsSets vectorVal [div (x * (x)) 2 | x <- [1..100]] 
+    assertNotEmpty []
+    assertEmpty vectorVal
+    assertElem 2 vectorVal
