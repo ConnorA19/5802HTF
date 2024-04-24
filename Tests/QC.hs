d6e3863c72604e03204288e07c87e89e367d0848
Physicoder <Physicoder@users.noreply.github.com>
2024-04-23 19:01:04 -0500

    Expanded QuickCheck Testing

--- Tests/QC.hs
+++ Tests/QC.hs
@@ -7,3 +7,8 @@ import Test.Framework.TestInterface
 
 prop_Recurse :: [Int] -> Bool
 prop_Recurse n = (vectorMaker n + 1) == ((vectorMaker N) ++ (sum [1..(n+1)]))
+
+prop_Len :: [Int] -> Bool
+prop_Len n = (length (vectorMaker n)) == n
+
+prop_Len_1 n = (prop_Len n) ==> (prop_Len (n+1))
