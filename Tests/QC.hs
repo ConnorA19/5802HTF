faeb40bcba61deede49c0a28ae00a292d32bc8d7
Physicoder <Physicoder@users.noreply.github.com>
2024-04-26 19:25:04 -0500

    Replacement Signature

--- Tests/QC.hs
+++ Tests/QC.hs
@@ -5,8 +5,8 @@ import Tests.MatrixAdder
 import Test.Framework
 import Test.Framework.TestInterface
 
-prop_Recurse :: Int -> Bool
 prop_Recurse n = ((vectorMaker (n + 1)) == ((vectorMaker n) ++ (sum [1..(n+1)])))
+  where types = n::Int
 
 prop_Len :: Int -> Bool
 prop_Len n = (length (vectorMaker n)) == n
