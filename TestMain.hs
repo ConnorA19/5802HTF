7739a9d30629efdaa8e965cbb3207ee6bb4f3586
Connor Antony <connor.antony19@gmail.com>
2024-04-26 21:20:18 -0500

    fixed my dumb mistake

--- TestMain.hs
+++ TestMain.hs
@@ -1,14 +1,14 @@
-{-# OPTIONS_GHC -F -pgmF htfpp #-}
-module Main where
-
--- The main test program.
-
-import Test.Framework
-import Test.Framework.BlackBoxTest
-import {-@ HTF_TESTS @-} Tests.HUnit
-
-main :: IO ()
-main = htfMain htf_importedTests
-    -- do bbts <- blackBoxTests "bbt-dir" "../scripts/run-sample" ".num" defaultBBTArgs
-       -- htfMain (htf_importedTests ++ [makeTestSuite "bbts" bbts])
+-- {-# OPTIONS_GHC -F -pgmF htfpp #-}
+-- module Main where
+--
+-- -- The main test program.
+--
+-- import Test.Framework
+-- import Test.Framework.BlackBoxTest
+-- -- import {-@ HTF_TESTS @-} Tests.HUnit
+--
+-- main :: IO ()
+-- main = htfMain htf_importedTests
+--     -- do bbts <- blackBoxTests "bbt-dir" "../scripts/run-sample" ".num" defaultBBTArgs
+--        -- htfMain (htf_importedTests ++ [makeTestSuite "bbts" bbts])
 
