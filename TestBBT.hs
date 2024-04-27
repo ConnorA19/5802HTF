b282698cc7bff6099c0df8865ac81c60a0226c14
Connor Antony <connor.antony19@gmail.com>
2024-04-26 20:58:40 -0500

    idk

--- TestBBT.hs
+++ TestBBT.hs
@@ -11,6 +11,11 @@ main :: IO ()
 main =
     -- do bbts <- blackBoxTests "bbt-dir" "../scripts/run-sample" ".num" defaultBBTArgs
        -- htfMain (htf_importedTests ++ [makeTestSuite "bbts" bbts])
-    do bbts <- blackBoxTests "bbt-dir" "./scripts/runBBT" ".num" defaultBBTArgs
-       htfMain (htf_importedTests ++ [makeTestSuite "bbts" bbts])
+    -- do bbts <- blackBoxTests "bbt-dir" "./scripts/runBBT" ".num" defaultBBTArgs
+       -- htfMain (htf_importedTests ++ [makeTestSuite "bbts" bbts])
+     do bbts <- blackBoxTests "bbt-dir" "./dist-newstyle/build/x86_64-linux/ghc-9.4.8/HTF-Test-Talk-0.1.0.0/t/Main/build/Main/Main" ".num" defaultBBTArgs
+        htfMain (htf_importedTests ++ [makeTestSuite "bbts" bbts])
+
+
+-- ./dist-newstyle/build/x86_64-linux/ghc-9.4.8/HTF-Test-Talk-0.1.0.0/t/Main/build/Main/Main
 
