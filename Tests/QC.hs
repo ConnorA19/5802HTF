e247a6a11759724051418c82d80f060435f94425
Physicoder <Physicoder@users.noreply.github.com>
2024-04-26 19:39:52 -0500

    Fix prop_Len

--- Tests/QC.hs
+++ Tests/QC.hs
@@ -9,6 +9,6 @@ prop_addition :: Int -> Bool
 prop_addition n = (n + n) == (n * 2)
 
 prop_Len :: Int -> Bool
-prop_Len n = (length (vectorMaker n)) == n
+prop_Len n = or ((length (vectorMaker n)) == n) (n <= 0)
 
 prop_Len_1 n = (prop_Len n) ==> (prop_Len (n+1))
