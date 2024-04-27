2943ca163f0b968eb8939f165d28ef9fb9cf1859
Physicoder <Physicoder@users.noreply.github.com>
2024-04-26 19:16:39 -0500

    Removed Brackets

--- Tests/QC.hs
+++ Tests/QC.hs
@@ -5,10 +5,10 @@ import Tests.MatrixAdder
 import Test.Framework
 import Test.Framework.TestInterface
 
-prop_Recurse :: [Int] -> Bool
+prop_Recurse :: Int -> Bool
 prop_Recurse n = (vectorMaker n + 1) == ((vectorMaker N) ++ (sum [1..(n+1)]))
 
-prop_Len :: [Int] -> Bool
+prop_Len :: Int -> Bool
 prop_Len n = (length (vectorMaker n)) == n
 
 prop_Len_1 n = (prop_Len n) ==> (prop_Len (n+1))
